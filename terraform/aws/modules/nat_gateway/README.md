95fb44b974a628a7068253566b4ed9f5a33cc5c9
Max Williams <max.williams@deliveryhero.com>
2017-07-17 17:05:05 +0200

    Adding NAT gateway module (#3)

--- terraform/aws/modules/nat_gateway/README.md
+++ terraform/aws/modules/nat_gateway/README.md
@@ -1,5 +1,18 @@
 # NAT gateway
 
-Creates multi-AZ NAT gateways, EIPS, subnets and route table.
+Creates per AZ NAT gateways with elastic IPs, subnets and a route table.
 
-*Work in progress*
+## Example
+
+```
+module "nat_gateway" {
+  source                   = "github.com/deliveryhero/dhh-system-engineering/terraform/aws/modules/nat_gateway"
+  vpc_id                   = "${module.vpc1.vpc_id}"
+  vpc_public_subnet_ids    = ["${module.vpc1.vpc_public_subnet_ids}"]
+  nat_private_subnet_cidrs = [
+    "172.20.12.0/23",
+    "172.20.14.0/23",
+    "172.20.16.0/23"
+  ]
+}
+```
---
+++ terraform/aws/modules/nat_gateway/main.tf
@@ -0,0 +1,38 @@
+resource "aws_eip" "nat_gateway" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc   = true
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  count         = "${length(data.aws_availability_zones.available.names)}"
+  allocation_id = "${element(aws_eip.nat_gateway.*.id, count.index)}"
+  subnet_id     = "${element(var.vpc_public_subnet_ids, count.index)}"
+}
+
+resource "aws_route_table" "nat_private" {
+  vpc_id = "${var.vpc_id}"
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${element(aws_nat_gateway.nat_gateway.*.id, count.index)}"
+  }
+  tags {
+    "Name" = "nat_private"
+  }
+}
+
+resource "aws_subnet" "nat_private" {
+  count                   = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id                  = "${var.vpc_id}"
+  cidr_block              = "${element(var.nat_private_subnet_cidrs, count.index)}"
+  availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
+  map_public_ip_on_launch = false
+  tags {
+    "Name" = "nat_private ${element(split(",", "a,b,c"), count.index)}"
+  }
+}
+
+resource "aws_route_table_association" "nat_gateway_private" {
+  count          = "${length(data.aws_availability_zones.available.names)}"
+  route_table_id = "${aws_route_table.nat_private.id}"
+  subnet_id      = "${element(aws_subnet.nat_private.*.id, count.index)}"
+}
