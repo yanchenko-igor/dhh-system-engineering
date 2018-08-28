e2a585f300016ee3ae998549981106c22d20d629
Max Williams <max.williams@deliveryhero.com>
2018-08-28 17:44:21 +0200

    removing old module

--- terraform/aws/modules/aws_sso/README.md
+++
@@ -1,3 +0,0 @@
-# AWS SSO using Google
-
-Please use this module instead: https://github.com/deliveryhero/tf-aws-saml-sso
--- terraform/aws/modules/aws_sso/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_saml_provider" "google" {
-  name                   = "google"
-  saml_metadata_document = "${file(var.idp_data_file_path)}"
-}
